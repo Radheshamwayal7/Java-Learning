1a9ee303d29263d638366a1245ae48c470705ad2
RadheshamWayal <shamwayal7@gmail.com>
2026-06-29 13:17:51 +0530

    Add New Code

---
+++ task1pattern.java
@@ -0,0 +1,9 @@
+public class task1pattern {
+    public static void main(String[] args){
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+    
+}
