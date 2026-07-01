7c318c6b5a8eccc55ee1a469bbf4e194c63b3504
RadheshamWayal <shamwayal7@gmail.com>
2026-07-01 22:24:16 +0530

    Add New Java Programs

---
+++ forloop.java
@@ -0,0 +1,7 @@
+public class forloop {
+    public static void main(String[] args) {
+        for(int counter = 10 ; counter <= 100 ; counter = counter + 10){
+            System.out.println(counter);
+        }
+    }
+}
