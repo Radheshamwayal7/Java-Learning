0306ec2a6f3ecb7fd9bd520b0f7758a51bf9cd38
RadheshamWayal <shamwayal7@gmail.com>
2026-07-06 08:57:23 +0530

    Sum of Odd Numbers

---
+++ SumofODDno.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class SumofODDno {
+    public static int OddSum(int n) {
+
+        int sum = 0;
+
+        for(int i = 1; i <= n; i++) {
+            if(i % 2 != 0){
+                sum = sum + i;
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int OddSum = OddSum(n);
+        System.out.println("Sum of Odd NO ="+OddSum);
+        
+        sc.close();
+    }
+}
+
