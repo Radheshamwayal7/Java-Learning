ad6e7b97ca051097d72b90ab4b0882442f57934d
RadheshamWayal <shamwayal7@gmail.com>
2026-07-07 12:59:31 +0530

    Radius and Circumference

--- RadiusandCircumference.java
+++ RadiusandCircumference.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+
 public class RadiusandCircumference {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
+        System.out.println("Enter the Radius=");
+        double radius = sc.nextDouble();
+
+        double circumference = 2 * Math.PI * radius;
+
+        System.out.println("Circumference ="+circumference);
     }
 }
