d8345ad7c97fcb27723016a7c875dd4f925beee1
RadheshamWayal <shamwayal7@gmail.com>
2026-07-07 12:44:32 +0530

    Maximum of 2 Numbers

---
+++ MaximumNo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MaximumNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a>b){
+            System.out.println("Maximum"+a);
+            System.out.println("Minimum"+b);
+        }else{
+            System.out.println("Maximum"+a);
+            System.out.println("Minimum"+b);
+        }
+
+        sc.close();
+    }
+}
