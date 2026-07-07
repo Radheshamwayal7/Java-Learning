edea24b8b632022015cdcb51e12e9fa5c7c34928
RadheshamWayal <shamwayal7@gmail.com>
2026-07-07 21:11:14 +0530

    Array Concept  Learning

---
+++ Array1Practice.java
@@ -0,0 +1,18 @@
+public class Array1Practice {
+    public static void main(String[] args) {
+        
+        int marks[] = new int[5];
+
+         marks[0] = 88;
+         marks[1] = 99;
+         marks[2] = 44;
+         marks[3] = 68;
+         marks[4] = 88; 
+        
+        System.out.println("Marks="+marks[0]);
+        System.out.println("Marks="+marks[1]);
+        System.out.println("Marks="+marks[2]);
+        System.out.println("Marks="+marks[3]);
+        System.out.println("Marks="+marks[4]);
+    }
+}
---
+++ Array2Practice.java
@@ -0,0 +1,14 @@
+public class Array2Practice {
+    public static void main(String[] args) {
+        
+        int marks[] = new int[3];
+
+        marks[0] = 22;
+        marks[1] = 55;
+        marks[2] = 77;
+
+        for(int i = 0; i < marks.length; i++) {
+            System.out.println("Marks="+marks[i]);
+        }
+    }
+}
