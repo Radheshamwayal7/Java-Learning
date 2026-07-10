17551a9127771d9e2cac1000a8278252a87ea10e
RadheshamWayal <shamwayal7@gmail.com>
2026-07-10 09:59:57 +0530

    String Fun ComparTo

---
+++ StringFunCompareTo.java
@@ -0,0 +1,8 @@
+public class StringFunCompareTo {
+    public static void main(String[] args) {
+        String name1 = "Tony";
+        String name2 = "Tony";
+
+        System.out.println(name1.compareTo(name2));
+    }
+}
