cafa7aba038ce47f5543ed7842c705fd2e57128c
RadheshamWayal <shamwayal7@gmail.com>
2026-07-10 08:49:45 +0530

    String Concatenation

---
+++ StringConcatenation.java
@@ -0,0 +1,9 @@
+public class StringConcatenation {
+    public static void main(String[] args) {
+        
+        String name1 = "Tony";
+        String name2 = "Stark";
+
+        System.out.println(name1 + " " + name2);
+    }
+}
