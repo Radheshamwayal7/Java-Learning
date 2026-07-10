74d5824c60580c04a6924f2172de97e1be889adb
RadheshamWayal <shamwayal7@gmail.com>
2026-07-10 09:36:02 +0530

    string Function CharAt

---
+++ StringCharAtFun.java
@@ -0,0 +1,10 @@
+public class StringCharAtFun {
+    public static void main(String[] args) {
+        
+        String name = "Radhesham";
+
+        for(int i = 0; i < name.length(); i++){
+            System.out.println("CharAt:" + name.charAt(i));
+        }
+    }
+}
--- quizBaxinga.java
+++
@@ -1,9 +0,0 @@
-public class quizBaxinga {
-    public static void main(String [] args){
-        if(3 % 2==0){
-            System.out.println("Bazinga");
-        }else{
-            System.out.println("Not Bazinga");
-        }
-    }
-}
