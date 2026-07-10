4eb923c9c527303eb8f2280bf6883fd46e99b957
RadheshamWayal <shamwayal7@gmail.com>
2026-07-10 10:35:19 +0530

    String Fun Substring

---
+++ StringFunSubstring.java
@@ -0,0 +1,8 @@
+public class StringFunSubstring {
+    public static void main(String[] args) {
+        
+        String name = "Tony";
+
+        System.out.println("Substring :"+ name.substring(1,3));
+    }
+}
