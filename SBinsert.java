202e5344c057b605de2dfd586b51bf26a5280835
RadheshamWayal <shamwayal7@gmail.com>
2026-07-11 07:48:01 +0530

    SB Insert function

---
+++ SBinsert.java
@@ -0,0 +1,8 @@
+public class SBinsert {
+    public static void main(String[] args) {
+        
+        StringBuilder sb = new StringBuilder("Tony");
+        sb.insert(3, 'y');
+        System.out.println(sb);
+    }
+}
