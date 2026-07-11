d5eefced3cb8e5c1b95b3f3e89a2a3cfacf316fc
RadheshamWayal <shamwayal7@gmail.com>
2026-07-11 07:36:46 +0530

    StringBuilder User Input Ex

---
+++ SringBuilderUIex.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class SringBuilderUIex {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(sc.nextLine());
+        System.out.println(sb);
+
+        sc.close();
+    }
+}
