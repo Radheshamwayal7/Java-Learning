26b8313f7322bccc1a6e815e8b08c526d89c78a1
RadheshamWayal <shamwayal7@gmail.com>
2026-07-11 07:37:48 +0530

    SB setCharAt Fun EX

---
+++ SBsetCharAt.java
@@ -0,0 +1,10 @@
+public class SBsetCharAt {
+    public static void main(String[] args) {
+        
+        StringBuilder sb = new StringBuilder("Hello Ji");
+
+        sb.setCharAt(0, 'M');
+        System.out.println("StringBuilder :" + sb);
+
+    }
+}
