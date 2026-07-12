863a33031ffcb5899b4c978bb02914e9d6c9e5ad
RadheshamWayal <shamwayal7@gmail.com>
2026-07-12 09:43:20 +0530

    Ar Op Modulo Ex

---
+++ ArithmeticOpMod.java
@@ -0,0 +1,10 @@
+public class ArithmeticOpMod {
+    public static void main(String[] args) {
+        
+        int a = 32;
+        int b = 5;
+
+        int mod = a % b;
+        System.out.println("Modulo Of Two Number Is :" + mod);
+    }
+}
