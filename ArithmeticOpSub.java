6b16190eb872f35a7b5d029860379cdb3be1e54c
RadheshamWayal <shamwayal7@gmail.com>
2026-07-12 09:25:37 +0530

    Ar Op Subtraction Ex

---
+++ ArithmeticOpSub.java
@@ -0,0 +1,10 @@
+public class ArithmeticOpSub {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 30;
+
+        int sub = a - b;
+        System.out.println("Subtraction Of Two Numbers : " + sub);
+    }
+}
