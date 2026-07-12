ca83aa751c1a8989c5dc36e128b502cdc3757a63
RadheshamWayal <shamwayal7@gmail.com>
2026-07-12 09:36:47 +0530

    Ar Op Division Ex

---
+++ ArithmeticOpDiv.java
@@ -0,0 +1,10 @@
+public class ArithmeticOpDiv {
+    public static void main(String[] args) {
+        
+        int a = 40;
+        int b = 8;
+
+        int div = a / b;
+        System.out.println("Division Of Two Number Is:"+ div);
+    }
+}
