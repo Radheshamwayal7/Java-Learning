8647b163faef5e56e83f0cb00eeb051534c04302
RadheshamWayal <shamwayal7@gmail.com>
2026-07-12 10:05:31 +0530

    Ar Op unary Op Increment

---
+++ ArOpUnaryIN.java
@@ -0,0 +1,18 @@
+public class ArOpUnaryIN {
+    public static void main(String[] args) {
+        
+        // int a = 30;
+        // int b = 0;
+
+        // b = a++;
+        // System.out.println(a);
+        // System.out.println(b);
+
+        int a = 10;
+        int b = 0;
+
+        b = ++a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
