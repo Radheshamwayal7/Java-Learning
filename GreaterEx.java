2008bc5577ae4bf0610dae7c8c1fdd8e69de6cd8
RadheshamWayal <shamwayal7@gmail.com>
2026-07-13 08:18:48 +0530

    Relation Op > Greater Ex

---
+++ GreaterEx.java
@@ -0,0 +1,13 @@
+public class GreaterEx {
+    public static void main(String[] args) {
+        
+        int a = 44;
+        int b = 43;
+
+        if(a > b) {
+            System.out.println("A is Greater Than B.");
+        }else {
+            System.out.println("B is Greater Than A.");
+        }
+    }
+}
