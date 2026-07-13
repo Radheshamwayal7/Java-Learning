f952fb604af262b9b04411f5edf526d4a0202aba
RadheshamWayal <shamwayal7@gmail.com>
2026-07-13 08:09:19 +0530

    Relation Op != Ex

---
+++ NotEqualTo.java
@@ -0,0 +1,13 @@
+public class NotEqualTo {
+    public static void main(String[] args) {
+        
+        int a = 22;
+        int b = 22;
+
+        if(a != b) {
+            System.out.println("True");
+        }else {
+            System.out.println("False");
+        }
+    }
+}
