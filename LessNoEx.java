9f525c7003deaa7fe3bdf3273a7da4000a4ce83e
RadheshamWayal <shamwayal7@gmail.com>
2026-07-13 08:51:18 +0530

    Relational OP >= Ex

---
+++ LessNoEx.java
@@ -0,0 +1,13 @@
+public class LessNoEx {
+    public static void main(String[] args) {
+        
+        int a = 22;
+        int b = 20;
+
+        if(a < b) {
+            System.out.println("True");
+        }else {
+            System.out.println("False");
+        }
+    }
+}
