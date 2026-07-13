d074cfddda259d75e2d7cd053450604e5964c9b0
RadheshamWayal <shamwayal7@gmail.com>
2026-07-13 07:51:21 +0530

    Relation Operators == Ex

---
+++ EqualToEqualTo.java
@@ -0,0 +1,11 @@
+public class EqualToEqualTo {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 11;
+        if(a == b) {
+            System.out.println("The Numbers Are Equal");
+        }else {
+            System.out.println("The Numbers Are not Equal");
+        }
+    }
+}
