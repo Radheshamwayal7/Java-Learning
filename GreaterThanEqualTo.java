81ccd1314561ffe291e379b9f59bcc8aed455117
RadheshamWayal <shamwayal7@gmail.com>
2026-07-13 08:59:44 +0530

    Relational Operator

---
+++ GreaterThanEqualTo.java
@@ -0,0 +1,8 @@
+public class GreaterThanEqualTo {
+    public static void main(String[] args) {
+        
+        int a = 94;
+        int b = 68;
+        System.out.println(a >= b);
+    }
+}
--- LessNoEx.java
+++ LessNoEx.java
@@ -2,7 +2,7 @@ public class LessNoEx {
     public static void main(String[] args) {
         
         int a = 22;
-        int b = 20;
+        int b = 80;
 
         if(a < b) {
             System.out.println("True");
