c3d6ca052c6a04559cfa36ed25e7b7308ba9084a
RadheshamWayal <shamwayal7@gmail.com>
2026-07-13 09:35:02 +0530

    Logical OR || Ex

---
+++ LogicalOR.java
@@ -0,0 +1,10 @@
+public class LogicalOR {
+    public static void main(String[] args) {
+        
+        int a = 22;
+        int b = 34;
+        int c = 21;
+
+        System.out.println(a > b || c < a);
+    }
+}
