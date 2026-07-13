4a31baf9e74759b7abb87858c43fbc2c4484d32f
RadheshamWayal <shamwayal7@gmail.com>
2026-07-13 09:25:44 +0530

    Logical Op && Ex

---
+++ LogicalAND.java
@@ -0,0 +1,9 @@
+public class LogicalAND {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 40;
+
+        System.out.println(b>a && b<c);
+    }
+}
