0b84633fc9e0054a612b6981021f72bf6d098b95
RadheshamWayal <shamwayal7@gmail.com>
2026-07-14 08:44:25 +0530

    Bitwise Op Binary OR

---
+++ BinaryOR.java
@@ -0,0 +1,10 @@
+public class BinaryOR {
+    public static void main(String[] args) {
+        
+        int a = 88;
+        int b = 55;
+
+        System.out.println("Binary OR is:");
+        System.out.println(a | b);
+    }
+}
