a6ca7f598a22f740c9a657bdad4f32948168b450
RadheshamWayal <shamwayal7@gmail.com>
2026-07-15 20:09:24 +0530

    Assignment Op Add EqualTO Op

---
+++ AddEqualTo.java
@@ -0,0 +1,10 @@
+public class AddEqualTo {
+    public static void main(String[] args) {
+        
+        int a = 33;
+        int b = 34;
+
+        a += b;
+        System.out.println("Addition is:" + a);
+    }
+}
