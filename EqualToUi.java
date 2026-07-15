fee2ab105e5df16873c4358876ebef2aad2d3c85
RadheshamWayal <shamwayal7@gmail.com>
2026-07-15 20:06:03 +0530

    Assignment Op Equal To Userinput

---
+++ EqualToUi.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class EqualToUi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter The Number:");
+        int n = sc.nextInt();
+
+        System.out.println("N Is :" + n);
+        sc.close();
+    }
+}
