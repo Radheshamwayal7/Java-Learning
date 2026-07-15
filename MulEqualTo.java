f1e7e909b751eb6de1a9874a7c501268d69b7158
RadheshamWayal <shamwayal7@gmail.com>
2026-07-15 20:22:38 +0530

    Assignment Op Mul EqualTO

---
+++ MulEqualTo.java
@@ -0,0 +1,10 @@
+public class MulEqualTo {
+    public static void main(String[] args) {
+        
+        int a = 38;
+        int b = 2;
+
+        a *= b;
+        System.out.println("Multiplication Is :" + a);
+    }
+}
