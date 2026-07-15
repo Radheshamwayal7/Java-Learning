75bda62e4b3b55bfcba585f83dbf772c20ea26c7
RadheshamWayal <shamwayal7@gmail.com>
2026-07-15 19:49:52 +0530

    Binary Left Shift OP

---
+++ BinaryLeftShift.java
@@ -0,0 +1,10 @@
+public class BinaryLeftShift {
+    public static void main(String[] args) {
+        
+        int n = 5;
+        int pos = 2;
+
+        int num = n << pos;
+        System.out.println(num);
+    }
+}
