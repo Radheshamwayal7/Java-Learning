1ea18431849e7d07e0579a4442c5fc87380e2c89
RadheshamWayal <shamwayal7@gmail.com>
2026-07-15 20:16:11 +0530

    Assignment Op Sub Equal TO

---
+++ SubEqualTo.java
@@ -0,0 +1,10 @@
+public class SubEqualTo {
+    public static void main(String[] args) {
+        
+        int a = 44;
+        int b = 33;
+
+        a -= b;
+        System.out.println("Subtraction Is :" + a);
+    }
+}
