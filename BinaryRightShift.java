c32167e77f96ec62a412fa911912526b46e81cb0
RadheshamWayal <shamwayal7@gmail.com>
2026-07-15 19:56:11 +0530

    Binary Right Shift OP

---
+++ BinaryRightShift.java
@@ -0,0 +1,9 @@
+public class BinaryRightShift {
+    public static void main(String[] args) {
+        
+        int n = 3;
+        int pos = 1;
+        int num = n >> pos;
+        System.out.println(num);
+    }
+}
