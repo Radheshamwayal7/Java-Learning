1c048934709dd1d6b448aefa607028706aeed966
RadheshamWayal <shamwayal7@gmail.com>
2026-07-15 20:28:36 +0530

    Assignment Op Div EqualTo

---
+++ DivEqualTo.java
@@ -0,0 +1,10 @@
+public class DivEqualTo {
+    public static void main(String[] args) {
+        
+        int a = 88;
+        int b = 9;
+
+        a /= b;
+        System.out.println("Division Is :" + a);
+    }
+}
