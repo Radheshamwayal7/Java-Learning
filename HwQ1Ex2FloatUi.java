3547973fd8459f2916413cd2fb142f9320e177dd
RadheshamWayal <shamwayal7@gmail.com>
2026-07-16 19:41:08 +0530

    Float Userinput Ex

---
+++ HwQ1Ex2FloatUi.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class HwQ1Ex2FloatUi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter The Age :");
+        float age = sc.nextFloat();
+        System.out.println("The Age Is :" + age);
+        sc.close();
+    }
+}
