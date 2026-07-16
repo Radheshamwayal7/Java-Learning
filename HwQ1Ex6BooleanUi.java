dde9e908559fffa55e6c55fef8415105a827f9c9
RadheshamWayal <shamwayal7@gmail.com>
2026-07-16 20:17:26 +0530

    Boolean Userinput

---
+++ HwQ1Ex6BooleanUi.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class HwQ1Ex6BooleanUi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        boolean a = sc.nextBoolean();
+        System.out.println("Boolean Is :" + a);
+        sc.close();
+    }
+}
