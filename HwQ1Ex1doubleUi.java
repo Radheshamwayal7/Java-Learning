0036c7d0bc34803a65d6281bed3a0bc9d9345003
RadheshamWayal <shamwayal7@gmail.com>
2026-07-16 19:35:05 +0530

    Userinput double Ex

---
+++ HwQ1Ex1doubleUi.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class HwQ1Ex1doubleUi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter The Age :");
+        double age = sc.nextDouble();
+        System.out.println("The Age is:" + age);
+        sc.close();
+    }
+}
