bd26beca47edbe728aee6b5f5bf7570020af8c1d
RadheshamWayal <shamwayal7@gmail.com>
2026-07-16 20:54:44 +0530

    Radius Of Circle

---
+++ HwQ2RadiusOfCircle.java
@@ -0,0 +1,8 @@
+public class HwQ2RadiusOfCircle {
+    public static void main(String[] args) {
+        
+        double diameter = 20;
+        double radius = diameter / 2;
+        System.out.println("Radius Of a Circle is :" + radius);
+    }
+}
