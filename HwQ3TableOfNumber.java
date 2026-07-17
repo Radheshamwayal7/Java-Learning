bd3ad4a6bfa3710d68ed42399fe21c6c480600f2
RadheshamWayal <shamwayal7@gmail.com>
2026-07-17 08:03:19 +0530

    Hw Q3 Table of a Number

---
+++ HwQ3TableOfNumber.java
@@ -0,0 +1,16 @@
+public class HwQ3TableOfNumber {
+    public static void main(String[] args) {
+        
+        int no = 2;
+        System.out.println(no * 1);
+        System.out.println(no * 2);
+        System.out.println(no * 3); 
+        System.out.println(no * 4);
+        System.out.println(no * 5);
+        System.out.println(no * 6);
+        System.out.println(no * 7);
+        System.out.println(no * 8);
+        System.out.println(no * 9);
+        System.out.println(no * 10);
+    }
+}
