5ded9d55eadbd87afc139049fc13b2569cb95888
RadheshamWayal <shamwayal7@gmail.com>
2026-07-19 12:42:25 +0530

    Dry Run And Testing For Loop

---
+++ DryRunForLoop.java
@@ -0,0 +1,13 @@
+public class DryRunForLoop {
+    public static void main(String[] args) {
+        int n = 3;
+        for(int i = 1; i <= n; i++) {
+            System.out.println("Apna College");
+        }
+        // for(; ;) {
+        //     System.out.println("Apna College");
+        // }
+                //No intialization ,condition ,updation declared the Loop run
+                    //  until Your gets Full.
+    }
+}
