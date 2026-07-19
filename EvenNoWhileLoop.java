1cccd8ebea05cddec12f2ed50641ea0f0a664dac
RadheshamWayal <shamwayal7@gmail.com>
2026-07-19 12:30:55 +0530

    Even No till n Using While Loop

---
+++ EvenNoWhileLoop.java
@@ -0,0 +1,12 @@
+public class EvenNoWhileLoop {
+    public static void main(String[] args) {
+        
+        int n = 34;
+        int i = 2;
+
+        while(i <= n) {
+            System.out.println(i);
+            i = i +2;
+        }
+    }
+}
