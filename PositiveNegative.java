c266e222c029493b12b84aa4579eae9271cf7e52
RadheshamWayal <shamwayal7@gmail.com>
2026-07-24 21:32:45 +0530

    Positive Negative Ex

---
+++ PositiveNegative.java
@@ -0,0 +1,14 @@
+class PosNeg {
+    public static void main(String[] args) {
+        
+        int n = -3;
+
+        if(n >= 0) {
+            System.out.println("The Number Is Positive.");
+        }else if(n == 0){
+            System.out.println("The Number Is Zero.");
+        }else{
+            System.out.println("The Number Is Negative.");
+        }
+    }
+}
