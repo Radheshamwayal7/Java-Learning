c34ee1a20a7261d3aa701a2ba92b984cde46fb40
RadheshamWayal <shamwayal7@gmail.com>
2026-07-25 17:47:46 +0530

    Inheritence Userinput Ex

---
+++ InheritenceExUi.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class Inheritence {
+    String name;
+    int age;
+
+    public void Print(){
+        System.out.println("The Name Is :"+ name);
+        System.out.println("The Age Is :"+ age);
+    }
+}
+
+class StudentInfo extends Inheritence {
+
+}
+
+public class InheritenceExUi {
+    public static void main(String[] args) {
+        StudentInfo St = new StudentInfo();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter The Name:");
+        St.name = sc.nextLine();
+        System.out.println("Enter The Age:");
+        St.age = sc.nextInt();
+
+        St.Print();
+        sc.close();
+    }
+}
+
