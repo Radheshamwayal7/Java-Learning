072f7831c58616a2fecd9968340d50dce04808d1
RadheshamWayal <shamwayal7@gmail.com>
2026-07-25 17:31:37 +0530

    Polymorphism Userinput Ex

---
+++ PolymorphismExUi.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class PolyMor {
+    String name;
+    int age;
+
+    public void PrintInfo() {
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+
+    public void PrintInfo(String name) {
+        System.out.println(this.name);
+
+    }
+
+    public int PrintInfo(int age) {
+        System.out.println(age);
+        return age;
+    }
+
+    public void PrintInfo(String name , int age) {
+        System.out.println(name);
+        System.out.println(age);
+    }
+
+}
+
+public class PolymorphismExUi {
+    public static void main(String[] args) {
+        
+        PolyMor pm  = new PolyMor();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Name:");
+        pm.name = sc.nextLine();
+        System.out.println("Enter Age:");
+        pm.age = sc.nextInt();
+
+        System.out.println("The Output Is :");
+        pm.PrintInfo();
+        sc.close();
+    }
+}
