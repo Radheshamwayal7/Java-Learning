0782f33e2a730b63c4b37fe4a2b48ae3d805b46f
RadheshamWayal <shamwayal7@gmail.com>
2026-07-26 23:04:04 +0530

    Added Package Demo

---
+++ PackageDemo/src/App.java
@@ -0,0 +1,10 @@
+import mypackage.Student;
+
+public class App {
+    public static void main(String[] args) {
+
+        Student s = new Student();
+        s.display();
+
+    }
+}
---
+++ PackageDemo/src/mypackage/Student.java
@@ -0,0 +1,7 @@
+package mypackage;
+
+public class Student {
+    public void display() {
+        System.out.println("Hello from Student Class");
+    }
+}
