82f76c98ad552a02813415b261550842c00ac379
RadheshamWayal <shamwayal7@gmail.com>
2026-07-27 19:39:51 +0530

    Abstraction Animal Ex using abstract

--- Abstraction.java
+++ Abstraction.java
@@ -2,13 +2,13 @@ abstract class Animal1 {
     abstract void walk();
 }
 
-class Horse extends Animal1 {
+class Horse2 extends Animal1 {
     public void walk() {
         System.out.println("Horse Walk on 4 Legs");
     }
 }
 
-class Chicken extends Animal1 {
+class Chicken2 extends Animal1 {
     public void walk() {
         System.out.println("Chicken walk on 2 Legs");
     }
@@ -16,7 +16,7 @@ class Chicken extends Animal1 {
 
 public class Abstraction {
     public static void main(String[] args) {
-        Horse H = new Horse();
+        Horse2 H = new Horse2();
         H.walk();
     }
 }
