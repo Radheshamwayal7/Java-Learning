445d7a3bda97ea8f60a724d817761b4d25902ec4
RadheshamWayal <shamwayal7@gmail.com>
2026-08-02 20:58:08 +0530

    Practice Calc

---
+++ PracticeCalc.java
@@ -0,0 +1,14 @@
+public class PracticeCalc {
+    public static void main(String[] args) {
+        
+        int a = 22;
+        int b = 44;
+
+        System.out.println("Multiplication :" + (a * b));
+        System.out.println("Addition :" + (a + b));
+        System.out.println("Subtraction :" + (a - b));
+        System.out.println("Division :" + a / b);
+        System.out.println("Modulo :" + (b%a));
+        System.out.println("Modulo :" + (a%b));
+    }
+}
